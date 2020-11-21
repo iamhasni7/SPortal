b1d75e45309ff6da0049fd4aa4080d020f4b242c
iamhasni7 <hasni.rock@yahoo.com>
2020-11-22 02:41:58 +0500

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/',(req,res) =>{
+  res.render('index',
+  {
+    lat: 37.33182,
+    lon: -122.03118,
+    uuid: "UUID"
+
+   })
+})
+app.get('/uuid/:uuid/lat/:lat/lon/:lon',(req,res)=>{
+  res.render('index',
+  {
+    lat: req.params.lat,
+    lon: req.params.lon,
+    uuid: req.params.uuid
+  })
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
